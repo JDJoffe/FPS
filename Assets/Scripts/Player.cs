cf72631765e65cac3bb1d16cece22c6864fd0f33
Joshua <joshuadjoffe1@gmail.com>
2019-05-01 10:26:23 +1000

    2ndpush
    hehe

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+#region Var
+    //variables
+    public float runSpeed = 7.5f;
+    public float walkSpeed = 6f;
+    public float gravity = 10f;
+    public float crouchSpeed = 4f;
+    public float jumpHeight = 20f;  
+    public float groundRayDistance = 1.1f;
+#endregion
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
