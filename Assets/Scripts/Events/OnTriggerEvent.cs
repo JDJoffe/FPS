771bfe933bbaffce103cf884d9f7188d1bcd4978
JDJoffe <48034855+JDJoffe@users.noreply.github.com>
2019-05-08 11:22:15 +1000

    movement

---
+++ Assets/Scripts/Events/OnTriggerEvent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Events;
+public class OnTriggerEvent : MonoBehaviour
+{
+    //reference tag to detect collisions
+    public string hitTag;
+    public UnityEvent onEnter;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if hitting hit tag or hitTag is set to nothing
+        if (other.tag == hitTag ||  hitTag == "")
+        {
+            //Invoke 
+            onEnter.Invoke();
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,24 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//remember how to do this dumby
+[RequireComponent(typeof(CharacterController))]
 public class Player : MonoBehaviour
 {
-#region Var
+    #region Var
     //variables
-    public float runSpeed = 7.5f;
+    public LayerMask groundLayer;
+    public float runSpeed = 8f;
     public float walkSpeed = 6f;
-    public float gravity = 10f;
-    public float crouchSpeed = 4f;
-    public float jumpHeight = 20f;  
+    public float gravity = -15f;
+    //public float crouchSpeed = 4f;
+    public float jumpHeight = 8f;
     public float groundRayDistance = 1.1f;
+    public Vector3 moveDirec;
+    public CharacterController charController;
+    private bool isJumping;
     #endregion
 
     #region start
     // Start is called before the first frame update
     void Start()
     {
-
+        charController = GetComponent<CharacterController>();
     }
     #endregion
 
@@ -26,8 +31,120 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Vector3 setup x y axis
 
+
+
+        #region AllInUpdateMovement
+        //moveDirec = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        ////base speed
+        //moveDirec *= walkSpeed;
+        //moveDirec = transform.TransformDirection(moveDirec);
+
+        ////sprinting
+
+        //if (Input.GetKeyDown("LShift"))
+        //{
+        //    moveDirec *= runSpeed;
+        //}
+
+        //if (charController.isGrounded)
+        //{
+
+        //    //jumping
+        //    if (Input.GetButton("Jump"))
+        //    {
+        //        // moveDirec.y is equal to jumpHeight
+        //        moveDirec.y = jumpHeight;
+        //    }
+        //}
+        //moveDirec.y -= gravity * Time.deltaTime;
+        ////we then tell the character Controller that it is moving in a direction timesed Time.deltaTime
+        //charController.Move(moveDirec * Time.deltaTime);
+        #endregion
+
+
+       
+        float inputH = Input.GetAxis("Horizontal");
+        float inputV = Input.GetAxis("Vertical");
+
+        //keep speed consistant, speed is not increased when moving diagonal
+        Vector3 normalized = new Vector3(inputH, 0f, inputV);
+        normalized.Normalize();
+        //move = inputH, inputV
+        Move (normalized.x, normalized.z);
+
+        //jump when on ground and when button pressed
+        if (IsGrounded() && Input.GetButtonDown("Jump"))
+        {
+            //jump
+            Jump();
+        }
+
+        //cant jump in air
+        if (!IsGrounded() && isJumping)
+        {
+            isJumping = false;
+        }
+
+        //grav acceleration set to 0 if jumping or grounded
+        if (IsGrounded() && !isJumping)
+        {
+
+            moveDirec.y = 0f;
+        }
+
+
+        moveDirec.y += gravity * Time.deltaTime;
+        charController.Move(moveDirec * Time.deltaTime);
+    }
+    #endregion
+    //move player in given direction horiz/verti
+    public void Move(float inputH, float inputV)
+    {
+        Vector3 direction = new Vector3(inputH, 0f, inputV);
+        // convert local direction to worldspace
+        direction = transform.TransformDirection(direction);
+        moveDirec.x = direction.x * walkSpeed;
+        moveDirec.z = direction.z * walkSpeed;
+
+        //if (Input.GetKeyDown("Shift"))
+        //{
+        //    Sprint();
+        //}
     }
 
-    #endregion
+
+    //testGrounded
+    private bool IsGrounded()
+    {
+        Ray groundRay = new Ray(transform.position, -transform.up);
+
+        //return Physics.Raycast(groundRay, groundRayDistance)
+
+        //raycast perform
+        if (Physics.Raycast(groundRay, groundRayDistance))
+        {
+            //return true if hit
+            return true;
+        }
+
+        //else return false
+        return false;
+
+        //return exits the function 
+    }
+
+    public void Sprint()
+    {
+        
+    }
+   public void Jump()
+    {
+        moveDirec.y = jumpHeight;
+        isJumping = true;
+    }
+    
+    
 }
